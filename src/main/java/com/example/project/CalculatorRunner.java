da9722d3e02ddb9ab0970e1b330491968ec6e808
cherinesoe <148895697+cherinesoe@users.noreply.github.com>
2024-09-24 13:06:50 +0000

    partner challenge

--- src/main/java/com/example/project/CalculatorRunner.java
+++ src/main/java/com/example/project/CalculatorRunner.java
@@ -5,8 +5,21 @@ public class CalculatorRunner {
 
         //test your program here
         //1. create an instance of the calculator class
-
+        Calculator calc = new Calculator("TI84");
         //2. call any methods of that class to test
+        int num1 = 3;
+        int num2 = 5;
+        System.out.println(calc.performOperation("+", num1, num2));
+        if (calc.divisibleBy(num1, num2)) {
+            System.out.println(num1 + " is divisible by " + num2);
+        } else {
+            System.out.println(num1 + " is not divisible by " + num2);
+        }
+        
+        System.out.println(calc.coordinatePair(num1, num2));
 
+        System.out.println("Distance between " + num1 + " and " + num2 + " is " + calc.absoluteValue(num1, num2));
+
+        System.out.println(calc.info());
     }
 }
